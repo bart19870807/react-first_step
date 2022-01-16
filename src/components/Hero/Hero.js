58cbccd5407b485f8bf32716c3fdde52e7de727f
Bartlomiej Stachowski <bart.stachowski@gmail.com>
2022-01-16 23:46:01 +0000

    Added List an Hero

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Hero.scss';
+
+class Hero extends React.Component {
+  render() {
+    return (
+      <header className={styles.component}>
+        <h2 className={styles.title}>Thinks to do</h2>
+        <img className={styles.image} src={'./pexels-heloisa-vecchio-10447127.jpg'}></img>
+      </header>
+    )
+  }
+}
+
+export default Hero;
